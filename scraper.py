2f1b4f70d3ca0e3041b1e8e16fbb6bc81b05dc15
max <maxrichman@gmail.com>
2013-11-10 15:43:45 -0500

    max tide CSV added

--- scraper.py
+++ scraper.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import httplib2
 import json
+import csv
 
 
 
 data = []
 station_list = []
-full_data = []
 with open('station_all.json') as f:
     for line in f:
         data.append(json.loads(line))
@@ -17,15 +17,20 @@ for item in data[0]['td']:
 print station_list
 
 def write_to_file(full_data):
-    return full_data
+    with open("output.csv", "wb") as f:
+        writer = csv.writer(f)
+        writer.writerows(full_data)
+
 
 def parse_results(content):
+    full_data = []
     td_data = json.loads(content)
     #print td_data
-    full_data = [(td_data['station']['lat']),(td_data['station']['lng']),(td_data['station']['verbose_name']),(td_data['station']['station_id'])]
+    #full_data = [(td_data['station']['lat']),(td_data['station']['lng']),(td_data['station']['verbose_name']),(td_data['station']['station_id'])]
     for i,item in enumerate(td_data['time']):
-        full_data += [(td_data['station']['lat']),(td_data['station']['lng']),(td_data['station']['verbose_name']),(td_data['station']['station_id']),(item), (td_data['series'][0]['values'][i])]
-    write_to_file(full_data)
+        full_data += [[td_data['station']['lat'],td_data['station']['lng'],td_data['station']['verbose_name'],td_data['station']['station_id'],item, td_data['series'][0]['values'][i]]]
+    #print full_data
+    return write_to_file(full_data)
 
 
 def page_load(url): #function to scrape websites
